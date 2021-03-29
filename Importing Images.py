1015a21e6b34ce524222d8f365c1dc512fb0e4e1
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2021-03-29 12:33:05 +0530

    Create Importing Images.py

---
+++ Importing Images.py
@@ -0,0 +1,13 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+path = 'ImagesAttendance'
+images = []     # LIST CONTAINING ALL THE IMAGES
+className = []    # LIST CONTAINING ALL THE CORRESPONDING CLASS Names
+myList = os.listdir(path)
+print("Total Classes Detected:",len(myList))
+for x,cl in enumerate(myList):
+        curImg = cv2.imread(f'{path}/{cl}')
+        images.append(curImg)
+        className.append(os.path.splitext(cl)[0])
